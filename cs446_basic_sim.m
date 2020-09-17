2c72f8b84eb7063ca0bd43bbb80524c1a48bf4f5
Lauren Niemiec <44628884+lniemiec@users.noreply.github.com>
2020-09-17 00:40:37 -0400

    Update cs446_basic_sim.m
    Started working through and adding the cell behavior, but haven't figure out the movement portion or handled all of the cases yet.

--- cs446_basic_sim.m
+++ cs446_basic_sim.m
@@ -1,8 +1,8 @@
 % CS446 -- Computational Modeling and Simulation II
+% Owen Goldthwaite, Gautam Mitra, Lolo Niemiec
 % September 15, 2020
-%
-% Initial Simulation for mutualism
-%
+% Initial Simulation for Mutualism
+
 
 %% Simulation Parameters %%%
 
@@ -10,10 +10,10 @@
 rng_set = rng(1234567);
 
 % Time-related variables
-dt = 1; % timestep
-simLength = 100; % length of simulation
+dt = 1;             % timestep, increment by days
+simLength = 360;    % length of simulation: 1 year
 numIterations = 1 + simLength/dt;
-animation_fps = 5; % Speed of visuazation
+animation_fps = .5;  % Speed of visuazation
 
 % Grid dimensions
 row_count = 100; % width
@@ -22,12 +22,16 @@ col_count = 100; % length
 %% Constants %%
 EMPTY = 1;
 PLANT = 2; 
-ANIMAL = 3;
+GROWING_PLANT = 3;
+ANIMAL = 4;
+POLLINATED_ANIMAL = 5;
 
 prob_init_plant = 0.01; % initial probability a cell is plant
 prob_init_animal = 0.1; % initial probability a cell is animal
+prob_death_plant = 0.01; % probability plant death at each timestep
+prob_pollination = 0.01; % probability animal will pick up/drop pollen
 
-animal_per_plant_value = 2; % Basically how many animals one plant can provide for
+animal_per_plant_value = 2; % How many animals one plant can provide for
 
 
 %% Set up grids
@@ -36,14 +40,15 @@ grids = ones(row_count, col_count, numIterations) * EMPTY;
 
 for row = 1:row_count
     for col = 1:col_count
-        % grid initialization, plants first
+        % Grid initialization, plants first
         plant_chance = rand;
         animal_chance = rand;
-        % Checking for tr33s
+        
+        % Get random number, if smaller than p, add a plant
         if plant_chance < prob_init_plant
             grids(row, col, 1) = PLANT;
 
-        % Now checking for animals
+        % Now adding animals if smaller than p
         elseif animal_chance < prob_init_animal
             grids(row, col, 1) = ANIMAL;
         end
@@ -58,14 +63,15 @@ for frame = 2:numIterations
     %% Absorbing boundary condition
     % Create grids that are the size of the grid + 2 on each side
     extended_grid_size = size(grids( : , : , frame-1))+2;
-    extended_grid = ones(extended_grid_size) * EMPTY; % initialize all as empty
+    % Initialize all of the cells as empty
+    extended_grid = ones(extended_grid_size) * EMPTY; 
     
-    % Set the inside portion of the grids equal to the corresponding values from
-    % the previous timestep (a.k.a the previous frame)
+    % Set the inside portion of the grids equal to the corresponding values 
+    % from the previous timestep (a.k.a the previous frame)
     extended_grid(2:end-1, 2:end-1) = grids(:,:,frame-1);
     
     %% Loop for updating each cell in the grid
-    % Loop over the indices corresponding to the original (non-extended) grid
+    % Loop over the indices corresponding to original(non-extended) grid
     for row = 2:row_count + 1
         for col = 2:col_count + 1
 
@@ -93,55 +99,121 @@ for frame = 2:numIterations
             plant_count = sum(neighbors == PLANT);
             animal_count = sum(neighbors == ANIMAL);
 
-            % Default setting of next updated cell to empty if it doesnt fit
+            % Default setting next updated cell to empty if it doesnt fit
             % any of these scenarios
             if(current_cell == EMPTY)
-                updated_cell = EMPTY;
-            end
+                updated_cell = EMPTY;                
+ 
+% Do we need this?
+%             % If current cell empty and have neighboring plants and animals
+%             if(current_cell == EMPTY && plant_count~=0 && animal_count~=0)
+% 
+%                 % Cell becomes plant if more plants 
+%                 if(plant_count > animal_count)
+%                     updated_cell = PLANT;
+%                 % Cell becomes animal if more animals
+%                 elseif(animal_count > plant_count)
+%                     updated_cell = ANIMAL;
+%                 % Cell randomly chooses if equal amt of plants and animals
+%                 elseif(animal_count == plant_count)
+%                     choice_num = rand;
+%                     if(choice_num < 0.5) % Random 50/50
+%                         updated_cell = PLANT;
+%                     else
+%                         updated_cell = ANIMAL;
+%                     end
+%                 end
+%             end  
+                                 
 
-
-            % If current cell is empty and there are some neighboring plants and animals
-            if(current_cell == EMPTY && plant_count ~= 0 && animal_count ~= 0)
-
-                % Cell becomes plant if more plants, animal if more animals or a random choice if equal
-                if(plant_count > animal_count)
-                    updated_cell = PLANT;
-                elseif(animal_count > plant_count)
-                    updated_cell = ANIMAL;
-                elseif(animal_count == plant_count)
-                    choice_num = rand;
-                    if(choice_num < 0.5) % Random 50/50
-                        updated_cell = PLANT;
-                    else
-                        updated_cell = ANIMAL;
-                    end
-                end
-            end
-
-            % If current cell is an plant then it will survive no matter what
-            % because it just needs sun to live
-            if(current_cell == PLANT)
-                updated_cell = PLANT;
-            end
-
-            % If current cell is an animal then it requires at least half as many
-            % plant neighbors as animal neighbors + 1. Essentially meaning one plant
-            % can provide sustenance for 2 animals, this value of 2 is changeable
-            if(current_cell == ANIMAL)
-                total_animal_count = animal_count + 1;
-                if(total_animal_count/animal_per_plant_value > plant_count)
+            % If current cell is a plant then there is a small chance for 
+            % it to die at any given timestep
+            elseif (current_cell == PLANT)
+                death = rand;             
+                if (death < prob_death_plant)
                     updated_cell = EMPTY;
                 else
-                    updated_cell = ANIMAL;
+                    updated_cell = PLANT;
                 end
-            end
+                
+            % If current cell is a growing plant then either use a 
+            % probability or multiple growth stages to test if it should 
+            % mature. There's a chance to die and it can't pollinate yet.
+            elseif (current_cell == GROWING_PLANT) 
+                death = rand;
+                if (death < prob_death_plant)
+                    updated_cell = EMPTY;
+                else
+                    growth_rate = .20;    % When should the plant grow
+                    probability_growth = rand;
+                    if (probability_growth < growth_rate)
+                        updated_cell = PLANT;
+                    else
+                        updated_cell = GROWING_PLANT;
+                    end
+                end
+
+            % If current cell is an animal then it requires at least half 
+            % as many plant neighbors as animal neighbors + 1. Essentially 
+            % meaning one plant can provide sustenance for 2 animals, this 
+            % value of 2 is changeable.
+%             if(current_cell == ANIMAL)
+%                 total_animal_count = animal_count + 1;
+%                 if(total_animal_count/animal_per_plant_value > plant_count)
+%                     updated_cell = EMPTY;
+%                 else
+%                     updated_cell = ANIMAL;
+%                 end
+                        
+%             end
+
+            % If current cell is an animal and it is near a plant, then 
+            % there is a chance it will pick up pollen. Then, it moves.
+            elseif (current_cell == ANIMAL)
+                total_animal_count = animal_count + 1;
+                if (plant_count >= 1)
+                    pollen_chance = rand;
+                    if (pollen_chance < prob_pollination)
+                        updated_cell = POLLINATED_ANIMAL;
+                    end
+                end
+                
+                % Move the animal to a new cell. If there aren't any plants
+                % or animals in the surrounding cells, then it can move to 
+                % any cell. If the surrounding cells are all occupied, the 
+                % animal dies.
+                num_neighbors = plant_count+animal_count;
+                if (num_neighbors == 0)
+                    % Move to any cell
+                elseif ( (0 < num_neighbors) && ...
+                        (num_neighbors < length(neighbors)) )
+                    % Move animal to limited # of cells
+                elseif (num_neighbors == length(neighbors))
+                    % Animal can't move, dies
+                    updated_cell = EMPTY;
+                end
+                            
+            % If current cell is a pollinated animal,and neighbors a 
+            % growing plant, there is a chance the animal pollinates that 
+            % plant and then loses the pollen
+            elseif (current_cell == POLLINATED_ANIMAL)
+                pollinate_prob = rand;
+                if ((pollinate_prob<prob_pollination) && (plant_count>=1))
+                    updated_cell = ANIMAL;
+                    % Pollinate the plant
+                else
+                    % Just keep the pollinated animal moving
+                end
+            
             
             % Updating next grid with the new cell value
             grids(row - 1, col - 1, frame) = updated_cell;
 
+            end
         end
     end
 end
+
 disp("All grids calculated");
 
 %% Visualize the grid
@@ -151,11 +223,13 @@ viz_fig = figure;
 viz_axes = axes(viz_fig);
 
 % Set the colors
-empty_color = [1, 1, 1];
-plant_color = [109/255, 188/255, 0];
-animal_color = [237/255 41/255 57/255];
+map = [ 1       1       1;          % Empty Cell: white
+        109/255 188/255 0;          % Plant Cell
+        237/255 41/255  57/255;     % Growing Plant Cell
+        .6       0      0;          % Animal Cell: grey blue
+        1       .8      .8];        % Pollinated Animal Cell: light blue
 
-colormap(viz_axes, [empty_color; plant_color; animal_color]); 
+colormap(viz_axes, map); 
 
 % Remove axis labels, make aspect ratio look good, and maintain that state
 axis off;
@@ -175,5 +249,3 @@ for i = 1:numIterations
 end
 
 disp("Simulation complete!");
-
-
